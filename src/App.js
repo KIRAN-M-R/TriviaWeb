8cc36000e01a33ba30aefa9a3e9ee976c5a60841
KIRAN M R <iamthekiranmr@gmail.com>
2022-11-03 10:26:46 +0530

    api call moved to home

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import { Result } from "postcss";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
-import Quiz from "./components/Quiz/Quiz";
 
-const App = (props) => {
-    console.log(props.hello);
+
+const App = () => {
+    
     return(
 
   <BrowserRouter>
     <Routes>
       <Route path="/" exact element={<Home />} />
-      <Route path="/quiz" exact element={<Quiz />} />
+      {/* <Route path="/quiz" exact element={<Quiz />} /> */}
       <Route path="/result" exact element={<Result />} />
     </Routes>
   </BrowserRouter>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,78 @@
-import React from 'react'
-import { fetchFromAPI } from '../../api';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import Quiz from '../Quiz/Quiz';
-const st = 'Take quiz >';
-const Home = () => {
-  
-  
-  
-  const navigate = useNavigate();
-  const handleNavigate = () => {
-    
-    
-  navigate(`/quiz`)
-  }
-  
-  return (
-    <div className="bg-slate-800 h-screen flex flex-row justify-center">
-      <div className=' flex flex-col justify-center space-y-56'>
-      <h1 className='font-mono text-8xl text-white' style={{}}>Trivia App</h1>
-        <button onClick={handleNavigate} className='font-mono text-2xl h-20 text-white bg-sky-900' >{st}</button>
-      </div>
-        
-      
-    </div>
-  )
-}
+import React from "react";
+import { fetchFromAPI } from "../../api";
 
-export default Home
+import { useState } from "react";
+import Quiz from "../Quiz/Quiz";
+import { useEffect } from "react";
+const st = "Take quiz >";
+
+const Home = () => {
+  const [navigateToQuiz, setNavigateToQuiz] = useState(false);
+
+  const [questions, setQuestions] = useState([]);
+  const [rightAnswers, setrightAnswers] = useState([]);
+  const [wrongAnswers, setwrongAnswers] = useState([]);
+
+  const data = [];
+  const handleNavigate = () => {
+    fetchFromAPI().then((response) => {
+      const { results } = response;
+      console.log(results);
+
+      console.log("hiiooo");
+      results?.map((result) => {
+        data.push({
+          question: result?.question,
+          incorrectAnswers: result?.incorrect_answers,
+          correctAnswer: result?.correct_answer,
+        });
+      });
+      console.log(data);
+      console.log("doi");
+
+      data.map((obj) => {
+        setQuestions((questions) => [...questions, obj?.question]);
+        const wr = obj?.incorrectAnswers;
+        setwrongAnswers((wrongAnswers) => [...wrongAnswers, wr]);
+        setrightAnswers((rightAnswers) => [
+          ...rightAnswers,
+          obj?.correctAnswer,
+        ]);
+      });
+
+      setNavigateToQuiz(true);
+    });
+
+    console.log("wh" + navigateToQuiz);
+  };
+/* useEffect(()=>{
+  setNavigateToQuiz(false);
+},[]) */
+  return (
+    <>
+      {navigateToQuiz ? (
+        <Quiz
+          questions={questions}
+          wrongAnswers={wrongAnswers}
+          rightAnswers={rightAnswers}
+        />
+      ) : (
+        <div className="bg-slate-800 h-screen flex flex-row justify-center">
+          <div className=" flex flex-col justify-center space-y-56">
+            <h1 className="font-mono text-8xl text-white" style={{}}>
+              Trivia App
+            </h1>
+            <button
+              onClick={handleNavigate}
+              className="font-mono text-2xl h-20 text-white bg-sky-900"
+            >
+              {st}
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Home;
