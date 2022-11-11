8f57f2aac630c7ad57751dc9731ab94a57621398
KIRAN M R <iamthekiranmr@gmail.com>
2022-11-11 22:32:45 +0530

    Made responsive

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -72,33 +72,26 @@ console.log("opt1"+JSON.stringify(correctAnswers));
     <>
     {navigateToResults ? (<Result score={score} handleRetakeQuiz={handleRetakeQuiz} />):(<div className="relative">
       <Navbar handleRetakeQuiz={handleRetakeQuiz} />
-      <div className="overflow-auto hover:overflow-scrol bg-slate-800 h-screen justify-start flex md:flex-row flex-col p-4 fixed top-14 left-0 right-0">
-        <div className="md:w-1/4 w-auto h-1/4 md:h-auto ">
-          <h1 className="text-yellow-500 mb-5">Question {index + 1}</h1>
+      <div className=" bg-slate-800 h-screen justify-start flex md:flex-row flex-col p-4 fixed top-14 left-0 right-0">
+        <div className=" w-auto md:w-1/4 h-28 md:h-auto">
+          <h1 className="text-yellow-500">Question {index + 1}</h1>
           <h1 className="text-white">{question}</h1>
         </div>
-        <section className="md:w-3/4 w-auto h-3/4 md:h-auto flex flex-col">
-          <div className="flex flex-row m-0 w-auto justify-around">
-            {/* <div className="grid grid-cols-1 content-center">
-              <button onClick={handlePrevious} className="text-yellow-500">
-                Previous
-              </button>
-            </div> */}
-            <section className="flex flex-col">
-              <h1 className="text-left text-lg mb-5 font-semibold text-white">
+        <section className=" md:w-3/4 w-auto h-3/4 md:h-auto flex flex-col">
+          <div className="flex flex-row m-0 w-auto justify-center">
+            
+            <section className="flex flex-col w-auto">
+              <h1 className="pl-4 sm:pl-0 text-lg mb-5 font-semibold text-white">
                 Options
               </h1>
               {options[index]?.map((opt) => {
-                return <TailwindOptionButton opt={opt} />;
+                let optTrimed = opt.trim();
+                return <TailwindOptionButton opt={optTrimed} />;
               })}
             </section>
-            {/* <div className="grid grid-cols-1 content-center">
-              <button onClick={handleNext} className="bg-yellow-500 w-20 h-10">
-                Next
-              </button>
-            </div> */}
+            
           </div>
-          <div className="flex flex-row justify-center mt-10 ">
+          <div className="flex flex-row justify-center md:mt-10 ">
             <div className="flex flex-col">
             <div className="flex flex-row-2 justify-between h-10">
             
@@ -113,7 +106,7 @@ console.log("opt1"+JSON.stringify(correctAnswers));
           
             </div>
         
-          <div className="flex flex-row justify-center h-10 mt-20">
+          <div className="flex flex-row justify-center h-10 mt-2 md:mt-20">
             <button onClick={handleSubmit} className="font-mono text-2xl  h-10 w-80 text-white bg-sky-900">
               Submit
             </button>
@@ -135,10 +128,10 @@ console.log("opt1"+JSON.stringify(correctAnswers));
     return (
       <button
         onClick={()=>{handleOptions(opt)}}
-        className={`font-mono ${colcode} hover:bg-slate-600 text-2xl h-14 m-4 text-white mb-2 mx-2 w-96`}
+        className={`font-mono ${colcode} hover:bg-slate-600 h-14 m-4 text-white mb-2 mx-2 w-80 sm:w-96`}
       >
         <div className="flex flex-row p-2 justify-between">
-          <h1 className="text-lg">{opt}</h1>
+          <h1 className="text-sm">{opt}</h1>
           <div className="box-content w-3 h-3 border-slate-400 border-2 bg-slate-700 m-2">
             {colcode==='bg-slate-900 ring ring-violet-300'?<svg
               xmlns="http://www.w3.org/2000/svg"
